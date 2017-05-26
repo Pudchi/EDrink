ddfa6d3f0f44c5183de5945fa3ca57045f08af50
Pin-Lun <pudchi330@gmail.com>
2017-05-26 14:44:20 +0800

    Initial Commit

---
+++ app/src/main/java/com/dashwood/edrink/MainActivity.java
@@ -0,0 +1,13 @@
+package com.dashwood.edrink;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
