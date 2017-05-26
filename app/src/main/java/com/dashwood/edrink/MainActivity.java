5e548a6f4812df9162cfe5276b4adae97dc470ac
Pin-Lun <pudchi330@gmail.com>
2017-05-26 16:03:45 +0800

    Add Water Loading and Percent

--- app/src/main/java/com/dashwood/edrink/MainActivity.java
+++ app/src/main/java/com/dashwood/edrink/MainActivity.java
@@ -3,11 +3,17 @@ package com.dashwood.edrink;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.airbnb.lottie.LottieAnimationView;
+
 public class MainActivity extends AppCompatActivity {
+
+    LottieAnimationView water_anim;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        water_anim = (LottieAnimationView) findViewById(R.id.water_loading);
     }
 }
