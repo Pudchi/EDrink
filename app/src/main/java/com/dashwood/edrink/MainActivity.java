d18475014568d966c1ef15ed1480dc25bb98ca9b
Pin-Lun <pudchi330@gmail.com>
2017-05-30 00:55:48 +0800

    Adjust Main Activity. Add Profile Activity. Add new animation and custom font

--- app/src/main/java/com/dashwood/edrink/MainActivity.java
+++ app/src/main/java/com/dashwood/edrink/MainActivity.java
@@ -1,19 +1,41 @@
 package com.dashwood.edrink;
 
+import android.content.Intent;
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.airbnb.lottie.LottieAnimationView;
 
 public class MainActivity extends AppCompatActivity {
 
     LottieAnimationView water_anim;
+    Button goto_profile;
+    Typeface typeface_regular;
+    TextView water_percent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        typeface_regular = TypefaceProvider.getTypeFace(getApplicationContext(), "Circular_book.ttf");
+
         water_anim = (LottieAnimationView) findViewById(R.id.water_loading);
+        water_percent = (TextView) findViewById(R.id.water_percent);
+        water_percent.setTypeface(typeface_regular);
+        goto_profile = (Button) findViewById(R.id.profile_btn);
+        goto_profile.setTypeface(typeface_regular);
+
+        goto_profile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, ProfileActivity.class));
+            }
+        });
+
     }
 }
---
+++ app/src/main/java/com/dashwood/edrink/ProfileActivity.java
@@ -0,0 +1,37 @@
+package com.dashwood.edrink;
+
+import android.graphics.Typeface;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class ProfileActivity extends AppCompatActivity {
+
+    TextView name_lbl, gender_lbl, hw_lbl, goal_lbl, name_txt, gender_txt, hw_txt, goal_txt;
+    Typeface typeface_regular;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+
+        typeface_regular = TypefaceProvider.getTypeFace(getApplicationContext(), "Circular_book.ttf");
+
+        name_lbl = (TextView) findViewById(R.id.name_lbl);
+        name_lbl.setTypeface(typeface_regular);
+        gender_lbl = (TextView) findViewById(R.id.gender_lbl);
+        gender_lbl.setTypeface(typeface_regular);
+        hw_lbl = (TextView) findViewById(R.id.hw_lbl);
+        hw_lbl.setTypeface(typeface_regular);
+        goal_lbl = (TextView) findViewById(R.id.goal_lbl);
+        goal_lbl.setTypeface(typeface_regular);
+        name_txt = (TextView) findViewById(R.id.name_txt);
+        name_txt.setTypeface(typeface_regular);
+        gender_txt = (TextView) findViewById(R.id.gender_txt);
+        gender_txt.setTypeface(typeface_regular);
+        hw_txt = (TextView) findViewById(R.id.hw_txt);
+        hw_txt.setTypeface(typeface_regular);
+        goal_txt = (TextView) findViewById(R.id.goal_txt);
+        goal_txt.setTypeface(typeface_regular);
+    }
+}
