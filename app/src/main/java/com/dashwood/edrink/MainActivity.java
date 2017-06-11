aff6e401951f0a712774b36db5cadaefe4658654
Pin-Lun <pudchi330@gmail.com>
2017-06-11 17:01:11 +0800

    Connect to HC05

--- app/src/main/java/com/dashwood/edrink/MainActivity.java
+++ app/src/main/java/com/dashwood/edrink/MainActivity.java
@@ -5,11 +5,14 @@ import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothSocket;
 import android.content.Intent;
+import android.graphics.Interpolator;
 import android.graphics.Typeface;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Message;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -20,7 +23,10 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.UUID;
+import java.util.logging.Handler;
+import java.util.logging.LogRecord;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -30,13 +36,19 @@ public class MainActivity extends AppCompatActivity {
     TextView water_percent;
     private FirebaseAuth.AuthStateListener authListener;
     private FirebaseAuth auth;
-    //private FirebaseUser user;
     static boolean isConnected = false;
     ProgressDialog progress;
     String address = null;
     BluetoothSocket btSocket = null;
     BluetoothAdapter bluetoothAdapter = null;
     static final UUID myUUID = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB");
+    InputStream inStream = null;
+    byte delimiter = 10;
+    boolean stopWorker = false;
+    int readBufferPosition = 0;
+    byte[] readBuffer = new byte[1024];
+    android.os.Handler handler;
+    private StringBuilder sb = new StringBuilder();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -63,13 +75,11 @@ public class MainActivity extends AppCompatActivity {
 
         if (address == null)
         {
-            isConnected = false;
             Toast.makeText(getApplicationContext(), "HC-05 NOT Connected", Toast.LENGTH_SHORT).show();
 
         }
         else
         {
-            isConnected = true;
             new ConnectBT().execute();
             connect.setVisibility(View.INVISIBLE);
             disconnect.setVisibility(View.VISIBLE);
@@ -77,6 +87,28 @@ public class MainActivity extends AppCompatActivity {
 
 
 
+        handler = new android.os.Handler() {
+            @Override
+            public void handleMessage(Message msg) {
+                switch (msg.what) {
+                    case 1:
+                        byte[] reading = (byte[]) msg.obj;
+                        String string_input = new String(reading, 0, msg.arg1);
+                        sb.append(string_input);
+                        int endOfLineIndex = sb.indexOf(";");                            // determine the end-of-line
+                        if (endOfLineIndex > 0) {                                            // if end-of-line,
+                            String sbprint = sb.substring(0, endOfLineIndex);               // extract string
+                            sb.delete(0, sb.length());
+                            water_percent.setText(sbprint);
+
+                        }
+                        Log.d("TAG", "String:"+ sb.toString() +  "Byte:" + msg.arg1 + "...");
+                        break;
+
+
+                }
+            }
+        };
 
 
         auth = FirebaseAuth.getInstance();
@@ -144,15 +176,15 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    private void sendSignal ( String number ) {
+    /*private void sendSignal ( String number ) {
         if ( btSocket != null ) {
             try {
-                btSocket.getOutputStream().write(number.toString().getBytes());
+
             } catch (IOException e) {
 
             }
         }
-    }
+    }*/
 
     private void Disconnect () {
         if ( btSocket!=null ) {
@@ -175,7 +207,7 @@ public class MainActivity extends AppCompatActivity {
         @Override
         protected Void doInBackground (Void... devices) {
             try {
-                if ( btSocket==null || isConnected == true ) {
+                if ( btSocket==null || !isConnected ) {
                     bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
                     BluetoothDevice dispositivo = bluetoothAdapter.getRemoteDevice(address);
                     btSocket = dispositivo.createInsecureRfcommSocketToServiceRecord(myUUID);
@@ -194,16 +226,65 @@ public class MainActivity extends AppCompatActivity {
             super.onPostExecute(result);
 
             if (!ConnectSuccess) {
-                Toast.makeText(getApplicationContext(), "Connection Failed. Is it a SPP Bluetooth? Try again.", Toast.LENGTH_SHORT).show();
-                finish();
+                Toast.makeText(getApplicationContext(), "Connection Failed. Try again?", Toast.LENGTH_SHORT).show();
+
             } else {
                 Toast.makeText(getApplicationContext(), "Connected", Toast.LENGTH_SHORT).show();
                 isConnected = true;
+
             }
 
             progress.dismiss();
+            beginListenForData();
 
         }
+    }
+
+
+    public void beginListenForData()   {
+        try {
+            inStream = btSocket.getInputStream();
+        } catch (IOException e) {
+        }
+
+        Thread workerThread = new Thread(new Runnable()
+        {
+            public void run()
+            {
+                while(!Thread.currentThread().isInterrupted() && !stopWorker)
+                {
+                    try
+                    {
+                        int bytesAvailable = inStream.available();
+                        int bytes;
+                        if(bytesAvailable > 0)
+                        {
+                            byte[] packetBytes = new byte[bytesAvailable];
+
+
+                            while(true)
+                            {
+                                try {
+                                    bytes = inStream.read(packetBytes);
+                                    handler.obtainMessage(1, bytes, -1 , packetBytes).sendToTarget();
+                                }
+                                catch (IOException e)
+                                {
+                                    break;
+                                }
+                            }
+
+                        }
+                    }
+                    catch (IOException ex)
+                    {
+                        stopWorker = true;
+                    }
+                }
+            }
+        });
+
+        workerThread.start();
     }
 
     @Override
